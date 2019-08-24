6122817c7df37e062ccb018632590f9118627b8d
Keith Isham <kisham8899@gmail.com>
2019-08-24 21:38:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,11 +19,10 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-get '/multiply/:num1/:num2' do
-    @num = params[:num].to_i ** 2
-    @num.to_s
-    
-    erb :'/multiply/num1/num2'
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
     
     
     
