8fc3480a8837111eccb98ba6462e3a493cdc96d3
Keith Isham <kisham8899@gmail.com>
2019-08-24 21:27:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,47 +1,31 @@
+require_relative 'config/environment'
 
-	get '/reversename/:name' do
-	  status 200
-	  name = params[:name]
-	  name.reverse!
-	end
+class App < Sinatra::Base
 
-	get '/square/:number' do
-		status 200
-		number = params[:number]
-		m = number.to_i * number.to_i
-		m.to_s
-	end 
+  # This is a sample static route.
+  get '/hello' do
+    "Hello World!"
+  end
 
-	get '/say/:number/:phrase' do 
-		status 200
-		n = params[:number].to_i
-		phrase = params[:phrase] + "\n"
-		result = phrase * n
-		m = result[0..-2]
-	end 
+  # This is a sample dynamic route.
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
 
-	get '/say/:word1/:word2/:word3/:word4/:word5' do
-		status 200
-		m = params[:word1] +  " " + params[:word2] + " " + params[:word3] +  " " + params[:word4] +  " " + params[:word5] + "."
-	end 
+  # Code your final two routes here:
+  get "/goodbye/:name" do 
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
+get '/multiply/:num1/:num2' do
+    @num = params[:num].to_i ** 2
+    @num.to_s
+    
+    erb :'/multiply/num.html'
+  end
 
-	get '/:operation/:number1/:number2' do
-	operation = params[:operation]
-    number1 = params[:number1].to_i
-    number2 = params[:number2].to_i
-    if operation == "add"
-      answer = number1 + number2
-    elsif operation == "subtract"
-      answer = number1 - number2
-    elsif operation == "multiply"
-      answer = number1 * number2
-    elsif operation == "divide"
-      answer = number1 / number2
-    else
-      answer = "Unable to perform this operation"
-    end
-    answer.to_s
-	end 
 
 
 end
